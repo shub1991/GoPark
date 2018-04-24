dc6bc2fcd31153088c67e51d6ff3100518dc645a
Shubham Basu <shubham.basu991@gmail.com>
2018-04-23 22:31:46 -0400

    Chnages in the booking

--- includes/addBooking.php
+++ includes/addBooking.php
@@ -1,19 +1,28 @@
 <?php
     session_start();
     include_once 'connect_sql.php';
-    $cust_id = $_SESSION['Cust_id'];
-    $park_id = $_POST['park_id'];
-    $s_time = $_POST['s_time'];
-    $e_time = $_POST['e_time'];
+    $cust_id = (int)$_SESSION['Cust_id'];
+    $park_id = (int)$_POST['park_id'];
+    $s_time = str_replace(':','',$_POST['s_time']);
+    $e_time =str_replace(':','',$_POST['e_time']);
     $reg_no = mysqli_real_escape_string($conn,$_POST['reg_no']);
-    $price = $_POST['price']; 
-    
-    $sql ="INSERT INTO booking (s_time,
-    e_time, reg_no, price) VALUES ('$s_time','$e_time','$reg_no','$price')";
+    $price = (int)$_POST['price']; 
+    if($s_time>2400)
+    {
+        $s_time = $s_time/100;
+    }
+    if($e_time>2400)
+    {
+        $e_time = $e_time/100;
+    }
+    $sql ="INSERT INTO booking (Cust_id , park_id, s_time,
+    e_time, reg_no, price) VALUES ($cust_id,$park_id,$s_time,$e_time,'$reg_no',$price)";
     $result = mysqli_query($conn,$sql);
+     
     $resultCheck = mysqli_num_rows($result);
-    if($resultCheck<1){
-        header("location: ../index.php?error="+$cust_id+" "+$park_id+" "+$s_time+" "+$e_time+" "+$reg_no+" "+$price);
+    echo $sql;
+    if($resultCheck!=1){
+        header("location: ../index.php?error=some error");
             exit();
     }else{
         header("location: ../index.php?success=booking confirmed");
